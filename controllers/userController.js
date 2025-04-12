d94996f701cbcec75858a62968b4a8994a222e29
Omar <omar.a.feteeh@gmail.com>
2025-04-12 16:58:23 +0200

    feat: implement /me endpoint to retrieve current user's data

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,11 @@ exports.getAllUsers = factory.getAll(User);
 
 exports.getUser = factory.getOne(User);
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
