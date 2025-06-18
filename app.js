e9472d08b8abd67b5fe52c388bef9816cdbe7eaa
Omar-A-Elsayed <93584296+Omar-A-Elsayed@users.noreply.github.com>
2025-06-18 17:34:04 +0300

    feat: setup Pug layout and structure with MVC routing for views (#24)

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const { AppError } = require('./utils');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -49,10 +50,8 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
