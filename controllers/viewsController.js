30e65496ced9520827a52eb5ed4ea0b328053e4c
Omar <omar.a.feteeh@gmail.com>
2025-06-26 14:09:29 +0300

    feat: render dynamic tours on the overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const { catchAsync } = require('./../utils');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get Tour data from collection
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
