436c684142b2782df4daa69972c36d0126d6b964
Omar-A-Elsayed <93584296+Omar-A-Elsayed@users.noreply.github.com>
2025-08-20 01:07:19 +0300

    Feat/building the user account page (#32)
    * feat: rendering user account page
    
    * feat: add change password functionality

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,13 @@ import '../css/style.css';
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { displayMap } from './leaflet';
+import { updateSettings } from './updateSettings';
 
 const mapEl = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (mapEl) {
   const locations = JSON.parse(mapEl.dataset.locations);
@@ -22,3 +25,33 @@ if (loginForm)
   });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save--password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password',
+    );
+
+    document.querySelector('.btn--save--password').textContent =
+      'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
+}
